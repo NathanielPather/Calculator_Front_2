8d4de2d6de8a3651985f2755eae7d473c7a65861
Nathaniel Pather <nathaniel_pather@hotmail.com>
2022-01-27 21:47:40 +1000

    Add a single post request that doesn't do much

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import React, { useState } from "react";
+
 
 const btnValues = [
   [7, "8", "9", "/"],
@@ -12,10 +14,71 @@ const btnValues = [
   ["0", ".", "=", "+"]
 ];
 
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
 function App() {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if (calc.num.length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : calc.num % 1 === 0
+            ? Number(calc.num + value)
+            : calc.num + value,
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+  
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
+  const equalsClickHandler = () => {
+    const requestOptions = {
+      method: 'POST',
+  };
+  fetch('http://localhost:3000/post', requestOptions)
+      .then(response => response.json())
+      .then(res => console.log(res))
+      .then(r => <Screen value={r} />)
+  };
+
   return (
     <Wrapper>
-      <Screen value={0} />
+      <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
         {
           btnValues.flat().map((btn, i) => {
@@ -24,21 +87,21 @@ function App() {
                 key={i}
                 className={""}
                 value={btn}
-                onClick={() => {
-                  console.log(`${btn} clicked!`);
-                }}
+                onClick={
+                  btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                  ? signClickHandler
+                  : btn === "."
+                  ? commaClickHandler
+                  : numClickHandler
+                }
               />
             );
           })
         }
       </ButtonBox>
     </Wrapper>
-    /*
-    <form action="../../post" method="post" 
-              className="form">
-          <button type="submit">Connected?</button>
-        </form>
-    */
   );
 }
 
